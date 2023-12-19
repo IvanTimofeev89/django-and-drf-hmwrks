2f204a3f7510ae58ee1de8ad7cd467c793d7e683
Ivan Timofeev <timofeevis89@gmail.com>
2023-12-19 15:37:07 +0300

    Orm part2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_flags = []
+        for form in self.forms:
+            is_main = form.cleaned_data.get('is_main')
+            main_flags.append(is_main)
+        if main_flags.count(True) > 1:
+            raise ValidationError('Только один тэг может быть основным')
+        elif main_flags.count(True) == 0:
+            raise ValidationError('Выберите основной тэг')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    fields = ['tag', 'is_main']
+    formset = ScopeInlineFormset
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
+
+
+
