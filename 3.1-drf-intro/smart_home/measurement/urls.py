2a9f8095ea2d99f1a3de4f62e8834035fb39227b
Ivan Timofeev <timofeevis89@gmail.com>
2024-01-10 17:25:49 +0300

    drf intro homework

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from django.contrib import admin
 
-urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
-]
+from .views import RetrieveUpdateSensorApi, CreateMeasurementApi, ListCreateSensor
+
+urlpatterns = (
+    path('v1/sensors/', ListCreateSensor.as_view()),
+    path('v1/sensor/<int:pk>/', RetrieveUpdateSensorApi.as_view()),
+    path('v1/add_measurement/', CreateMeasurementApi.as_view()),
+
+)
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,26 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView, ListCreateAPIView
+
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer, SensorDetailSerializer
+
+
+# 1. Создать датчик. Указываются название и описание датчика.
+# 4. Получить список датчиков. Выдаётся список с краткой информацией по датчикам: ID, название и описание.
+class ListCreateSensor(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+# 2. Изменить датчик. Указываются название и описание.
+# 5.  Получить информацию по конкретному датчику. Выдаётся полная информация по датчику:
+class RetrieveUpdateSensorApi(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+
+# 3.Добавить измерение. Указываются ID датчика и температура.
+class CreateMeasurementApi(CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
